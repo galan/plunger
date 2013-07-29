c18a29550b7c44891c168a73bded5e7b57952ed1
Daniel Galán y Martins <git@galan.de>
2013-07-29 23:53:44 +0200

    amq

--- plunger-activemq-x/src/main/java/de/galan/plunger/command/activemq/AmqLsCommand.java
+++ plunger-activemq-x/src/main/java/de/galan/plunger/command/activemq/AmqLsCommand.java
@@ -1,5 +1,7 @@
 package de.galan.plunger.command.activemq;
 
+import static org.apache.commons.lang.StringUtils.*;
+
 import java.io.IOException;
 
 import javax.management.MBeanServerConnection;
@@ -14,6 +16,7 @@ import org.apache.activemq.broker.jmx.BrokerViewMBean;
 import de.galan.plunger.command.CommandException;
 import de.galan.plunger.command.generic.AbstractLsCommand;
 import de.galan.plunger.domain.PlungerArguments;
+import de.galan.plunger.domain.Target;
 import de.galan.plunger.util.Output;
 
 
@@ -30,7 +33,10 @@ public class AmqLsCommand extends AbstractLsCommand {
 	@Override
 	protected void initialize(PlungerArguments pa) throws CommandException {
 		try {
-			connector = JMXConnectorFactory.connect(new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi"));
+			Target target = pa.getTarget();
+			String port = defaultIfBlank(target.getParameterValue("jmxPort"), "1099");
+			String url = "service:jmx:rmi:///jndi/rmi://" + target.getHost() + ":" + port + "/jmxrmi";
+			connector = JMXConnectorFactory.connect(new JMXServiceURL(url));
 		}
 		catch (IOException ex) {
 			throw new CommandException("...", ex);
@@ -48,10 +54,9 @@ public class AmqLsCommand extends AbstractLsCommand {
 			BrokerViewMBean mbean = MBeanServerInvocationHandler.newProxyInstance(connection, mbeanName, BrokerViewMBean.class, true);
 
 			for (ObjectName on: mbean.getQueues()) {
-				System./**/out.println("queue: " + on.getCanonicalName());
+				printDestination(pa, on.getKeyProperty("destinationName"), 0, 0, true);
 			}
-
-			System./**/out.println("Id:" + mbean.getBrokerId());
+			//System./**/out.println("Id:" + mbean.getBrokerId());
 		}
 		catch (Exception ex) {
 			throw new CommandException("....", ex);
