349bee73eb3f2002efdc9914c321f32913b6e2aa
Daniel Galán y Martins <git@galan.de>
2016-01-11 08:51:32 +0100

    updated dependencies

--- plunger/src/test/java/de/galan/plunger/util/MessageMarshallerTest.java
+++ plunger/src/test/java/de/galan/plunger/util/MessageMarshallerTest.java
@@ -1,6 +1,6 @@
 package de.galan.plunger.util;
 
-import static org.assertj.core.api.StrictAssertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
